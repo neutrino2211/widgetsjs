1f076d2d07e3bcb7fc112a409d4508e13c0f4306
neutrino2211 <neutrino2211@gmail.com>
2021-06-25 20:53:17 +0100

    Added web friendly build

--- dist/index.js
+++ dist/index.js
@@ -3,16 +3,26 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 Object.defineProperty(exports, "__esModule", { value: true });
+//Always needs to be top
 var shim_1 = require("./shim");
 shim_1.shim();
+var functionalComponent_1 = require("./functionalComponent");
+var StatefulWidget_1 = require("./StatefulWidget");
+var StatelessWidget_1 = require("./StatelessWidget");
 var utils_1 = require("./utils");
-exports.useComponent = utils_1.useComponent;
-exports.getRef = utils_1.getRef;
+var utils_2 = require("./utils");
+exports.useComponent = utils_2.useComponent;
+exports.getRef = utils_2.getRef;
 var Widget_1 = require("./Widget");
 exports.Widget = Widget_1.Widget;
-var StatefulWidget_1 = require("./StatefulWidget");
-exports.StatefulWidget = StatefulWidget_1.StatefulWidget;
-var StatelessWidget_1 = require("./StatelessWidget");
-exports.StatelessWidget = StatelessWidget_1.StatelessWidget;
+var StatefulWidget_2 = require("./StatefulWidget");
+exports.StatefulWidget = StatefulWidget_2.StatefulWidget;
+var StatelessWidget_2 = require("./StatelessWidget");
+exports.StatelessWidget = StatelessWidget_2.StatelessWidget;
 __export(require("./functionalComponent"));
+window.StatefulWidget = StatefulWidget_1.StatefulWidget;
+window.StatelessWidget = StatelessWidget_1.StatelessWidget;
+window.useComponent = utils_1.useComponent;
+window.statefulFunctionWidget = functionalComponent_1.statefulFunctionWidget;
+window.statelessFunctionWidget = functionalComponent_1.statelessFunctionWidget;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
+//Always needs to be top
 import {shim} from "./shim"
 shim();
+
+
+
+import { statefulFunctionWidget, statelessFunctionWidget } from "./functionalComponent";
+import { StatefulWidget } from "./StatefulWidget";
+import { StatelessWidget } from "./StatelessWidget";
+import { useComponent } from "./utils";
+
 export {State, StateTransformers, useComponent, getRef} from "./utils";
 export {Widget} from './Widget'
 export {StatefulWidget} from './StatefulWidget'
 export {StatelessWidget} from './StatelessWidget'
 export * from "./functionalComponent"
+
+(window as any).StatefulWidget = StatefulWidget;
+(window as any).StatelessWidget = StatelessWidget;
+(window as any).useComponent = useComponent;
+(window as any).statefulFunctionWidget = statefulFunctionWidget;
+(window as any).statelessFunctionWidget = statelessFunctionWidget;
